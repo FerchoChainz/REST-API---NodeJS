3e99c3bf171e870e9a8529b6e1ccc77dfab5a17b
FerchoChainz <a21110132@ceti.mx>
2026-07-03 18:03:36 -0600

    Creating employees

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -1,8 +1,16 @@
-
+import {pool} from '../db.js';
 
 const getEmployees = (req, res)=> res.send('Obteniendo empleados');
 
-const createEmployee = (req, res)=> res.send('creando empleados');
+const createEmployee = async  (req, res)=> {
+    const {name, salary} = req.body;
+    const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)',[name, salary]);
+    res.send({
+        name,
+        salary,
+        id: rows.insertId
+    });
+};
 
 const updateEmployee = (req, res)=> res.send('actualizando empleados');
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import indexRoutes from './routes/index.routes.js';
 
 const app = express();
 
+
+app.use(express.json());
+
 app.use(indexRoutes);
 app.use(employeesRoutes);
 
