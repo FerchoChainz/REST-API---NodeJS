ca8c8b022ba0db9a58baab2ce256acb6f065b3c2
FerchoChainz <a21110132@ceti.mx>
2026-07-08 12:38:41 -0600

    Get employees API correct

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -1,14 +1,19 @@
 // To create routes we need to import the Router from express
 import { Router } from "express";
-import {getEmployees, createEmployee, updateEmployee, deleteEmployee} from '../controllers/employees.controllers.js';
+import {getEmployees, createEmployee, updateEmployee, deleteEmployee, getEmployee } from '../controllers/employees.controllers.js';
 
 const router = Router();
 
 // GET, POST, PUT, DELETE
 // A way better to read the code is to create a controller for each route, so we can separate the logic from the routes and make it more readable.
 router.get('/employees',getEmployees)
+
+router.get('/employees/:id',getEmployee)
+
 router.post('/employees',createEmployee)
+
 router.put('/employees', updateEmployee)
+
 router.delete('/employees', deleteEmployee)
 
 
