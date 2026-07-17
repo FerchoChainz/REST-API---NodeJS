02b2d451f57fc5b7c765d16994d9de7b7cea1a57
FerchoChainz <a21110132@ceti.mx>
2026-07-17 16:32:34 -0600

    Handling errors

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -1,64 +1,109 @@
-import {pool} from '../db.js';
+import { pool } from "../db.js";
 
-const getEmployees = async (req, res)=> {
-    const [rows] = await pool.query('SELECT * FROM employee');
+const getEmployees = async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM employee");
     res.json(rows);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something goes wrong",
+    });
+  }
 };
 
-const getEmployee = async (req, res) => { 
-    console.log(req.params.id);
-    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id]);
+const getEmployee = async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [
+      req.params.id,
+    ]);
 
-
-    if (rows.length <= 0){
-        return res.status(404).json({
-            message: 'Employee not found'
-        })
+    if (rows.length <= 0) {
+      return res.status(404).json({
+        message: "Employee not found",
+      });
     }
 
     res.json(rows);
-}
-
-const createEmployee = async  (req, res)=> {
-    const {name, salary} = req.body;
-    const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)',[name, salary]);
-    res.send({
-        name,
-        salary,
-        id: rows.insertId
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something goes wrong",
     });
+  }
 };
 
-const updateEmployee = async (req, res)=> {
-    const {id} = req.params;
-    const {name, salary} = req.body;
+const createEmployee = async (req, res) => {
+  const { name, salary } = req.body;
 
-    const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?', [name, salary, id]);
+  try {
+    const [rows] = await pool.query(
+      "INSERT INTO employee (name, salary) VALUES (?, ?)",
+      [name, salary],
+    );
+    res.send({
+      name,
+      salary,
+      id: rows.insertId,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something goes wrong",
+    });
+  }
+};
+
+const updateEmployee = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, salary } = req.body;
+
+    const [result] = await pool.query(
+      "UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?",
+      [name, salary, id],
+    );
     console.log(result);
 
-
-    if(result.affectedRows === 0){
-        return res.status(404).json({
-            message: 'Employee not found'
-        })
+    if (result.affectedRows === 0) {
+      return res.status(404).json({
+        message: "Employee not found",
+      });
     }
 
-    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id]);
+    const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [
+      id,
+    ]);
 
     res.json(rows[0]);
-}  
-
-const deleteEmployee = async (req, res)=> {
-    const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id]);
-    
-    if(result.affectedRows <= 0){
-        return res.status(404).json({
-            message:'Employee not found'
-        })
-    }
-    
-    res.sendStatus(204);
-
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something goes wrong",
+    });
+  }
 };
 
-export {getEmployees, createEmployee, updateEmployee, deleteEmployee, getEmployee};
+const deleteEmployee = async (req, res) => {
+    try{
+        const [result] = await pool.query("DELETE FROM employee WHERE id = ?", [
+    req.params.id,
+  ]);
+
+  if (result.affectedRows <= 0) {
+    return res.status(404).json({
+      message: "Employee not found",
+    });
+  }
+
+  res.sendStatus(204);
+    } catch (error) {
+    return res.status(500).json({
+      message: "Something goes wrong",
+    })
+  }
+};
+
+export {
+  getEmployees,
+  createEmployee,
+  updateEmployee,
+  deleteEmployee,
+  getEmployee,
+};
