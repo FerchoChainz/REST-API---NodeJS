f8e9ad69f579d662fce2e722a0ebd2599ddeef40
FerchoChainz <a21110132@ceti.mx>
2026-07-17 17:13:51 -0600

    JSON message to routes not found

--- src/index.js
+++ src/index.js
@@ -12,5 +12,11 @@ app.use(indexRoutes);
 app.use('/api',employeesRoutes);
 
 
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Endpoint not found'
+    })
+})
+
 
 app.listen(3000)
