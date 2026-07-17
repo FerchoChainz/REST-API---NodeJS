a2138c4e22d8d758a089309fc27cfba3289e0fac
FerchoChainz <a21110132@ceti.mx>
2026-07-17 15:08:38 -0600

    Delete employees done

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -31,6 +31,17 @@ const createEmployee = async  (req, res)=> {
 
 const updateEmployee = (req, res)=> res.send('actualizando empleados');
 
-const deleteEmployee = (req, res)=> res.send('elminando empleados');
+const deleteEmployee = async (req, res)=> {
+    const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id]);
+    
+    if(result.affectedRows <= 0){
+        return res.status(404).json({
+            message:'Employee not found'
+        })
+    }
+    
+    res.sendStatus(204);
+
+};
 
 export {getEmployees, createEmployee, updateEmployee, deleteEmployee, getEmployee};
